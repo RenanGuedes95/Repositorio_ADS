a2d329037a8380318bc91c1ae883ee664f543246
renanguedes95 <renan.brega@viannasempre.com.br>
2024-03-18 15:38:07 -0300

    adicionando o alerta para a menor e a maior forma possíveis

--- Tecnologia Web Avançada - JavaScript/Exercicio_Semana07_CriacaoDeBotoesFuncionais/JAVASCRIPT/javascript.js
+++ Tecnologia Web Avançada - JavaScript/Exercicio_Semana07_CriacaoDeBotoesFuncionais/JAVASCRIPT/javascript.js
@@ -6,6 +6,8 @@ botaoAumentar.addEventListener("click", () => {
         document.body.classList.replace("texto-regular", "texto-grande");
     } else if (document.body.classList.contains("texto-pequeno")) {
         document.body.classList.replace("texto-pequeno", "texto-regular");
+    }else{
+        window.alert("Este é o maior tamanho possível!!");
     }
 });
 
@@ -14,6 +16,8 @@ botaoDiminuir.addEventListener("click", () => {
         document.body.classList.replace("texto-regular", "texto-pequeno");
     } else if(document.body.classList.contains("texto-grande")){
         document.body.classList.replace("texto-grande", "texto-regular");
+    } else{
+        window.alert("Este é o menor tamanho possível!!");
     }
 });
 

7472d52b9f0f35083970ba3954e5583a4a7c464d
renanguedes95 <renan.brega@viannasempre.com.br>
2024-03-28 12:26:37 -0300

    .

--- Estrutura de Dados - C#/Lista_Exercicios_01/Exercicio02/obj/Debug/net8.0/Exercicio02.AssemblyInfo.cs
+++ Estrutura de Dados - C#/Lista_Exercicios_01/Exercicio02/obj/Debug/net8.0/Exercicio02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2a7e196d8269678c3ffeb101090a0034a3b4b17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2b017412a59e27eabe9264b9638cb62851c122f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

65370194bb5c898d478c10695b08572099853300
renanguedes95 <renan.brega@viannasempre.com.br>
2024-04-09 14:46:11 -0300

    Exercício para imprimir um vetor de inteiros separando cada elemento por um '-' (traço) utilizando recursividade.

---
+++ Estrutura de Dados - C#/Exercicios_Monitoria/Recursividade/Questao03/Program.cs
@@ -0,0 +1,20 @@
+﻿//Faça um procedimento recursivo que imprima todos os elementos de um vetor de inteiros separados por '-' (traço). Observação: traços só podem ser impressos se estiverem separando elementos do vetor, ou seja, não pode haver traço no início ou no final da impressão.
+
+using System;
+
+internal class Program{
+    
+    public static void imprimeVetor(int [] vetorInteiros, int indice = 0){
+        if(indice == vetorInteiros.Length - 1){
+            Console.Write($"{vetorInteiros[indice]}");
+        }else{
+            Console.Write($"{vetorInteiros[indice]} - ");
+            imprimeVetor(vetorInteiros, ++indice);
+        }
+    }
+    public static void Main(string[] args){
+        var vetorInteiros = new int []  { 1,2,3,4,5, 6, 7, 8, 9, 10, 0 };
+
+        imprimeVetor(vetorInteiros);
+    }
+}

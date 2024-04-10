a7a7f01c541b6b0f672d59495d1cd9874afe1781
renanguedes95 <renan.brega@viannasempre.com.br>
2024-04-10 15:11:17 -0300

    Calcular o fatoria de um número inteiro através de recursividade.

---
+++ Estrutura de Dados - C#/Exercicios_Extras/Exercício03_Recursividade/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Faça uma função recursiva que calcule e retorne o fatorial de um número inteiro N.*/
+
+using System;
+internal class Program{
+    public static void Main(string[] args){
+        Console.WriteLine(Fatorial(5));
+    }
+
+    public static int Fatorial(int num){
+        if(num == 0){
+            return 1;
+        }  else {
+            return num * Fatorial ( num - 1 );
+        }
+    }
+}

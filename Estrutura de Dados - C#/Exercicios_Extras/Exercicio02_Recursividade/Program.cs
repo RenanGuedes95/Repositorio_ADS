cc6e93243c6a1e08bc8ca0a2770b96c65d0491b8
renanguedes95 <renan.brega@viannasempre.com.br>
2024-04-10 15:02:21 -0300

    Exercício para inverter a ordem de um vetor através de recursividade

---
+++ Estrutura de Dados - C#/Exercicios_Extras/Exercicio02_Recursividade/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Dado um array de inteiros e o seu número de elementos, inverta a posição dos seus elementos.*/
+
+using System;
+internal class Program{
+    public static void Main(string[] args){
+        var arrayInteiros = new int [] { 1,2,3,4,1,6,7,8,9,10 };
+
+        ordenarVetor(arrayInteiros);
+
+    }
+    
+
+    public static void ordenarVetor(int [] arrayInteiros, int i = 0){
+        int j = arrayInteiros.Length - 1 - i;
+        if(i < j){
+            Troca(arrayInteiros, i, j);
+            ordenarVetor(arrayInteiros, ++i);
+        }    
+    }
+
+    public static void Troca(int [] arrayInteiros, int i, int j){
+        int aux = arrayInteiros[i];
+        arrayInteiros[i] = arrayInteiros[j];
+        arrayInteiros[j] = aux;
+    }
+}
